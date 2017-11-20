0236711f17c8caf88fe29d289f93a65a6173aadc
benttoenail <maxrose8841@gmail.com>
2017-11-20 08:59:47 +0100

    First Commit

---
+++ Assets/Scripts/BentParticles.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BentParticles : MonoBehaviour {
+
+    //Number of particles
+    #region Basic Properties
+
+    #endregion
+
+    //Center of emitter
+    //"Throttle"
+    #region Emitter Parameters
+
+    #endregion
+
+    //Life time, random Life
+    #region Particle Life Parameters
+
+    #endregion
+
+    //Advanced Parameters That I'll wait on
+    /*
+     Velocity Parameters
+     Acceleration Parameters
+     Rotation Parameters
+     Turbulent Noise Parameters
+         */
+
+    //Mesh data
+    //Material Initialization
+    //Shadows
+    #region Render Settings
+
+    #endregion
+
+    //Hard coding resources into Script
+    #region Built In Resources
+
+    #endregion
+
+    //Variables and properties that will drive particles
+    #region Private Variables and Properties 
+
+    #endregion
+
+    //Functions for updating the Kernal shader
+    //UpdateKernal
+    //Create buffers and Materials
+    //Reset all Resources
+    //Swapping buffers and management 
+    #region Resource Management 
+
+    #endregion
+
+
+    #region MonoBehaviour Functions
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #endregion
+}
